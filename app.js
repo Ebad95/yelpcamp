52668c87f7f12a8cf7b7a0f7eda22c61aea98812
Ebad husain <ebad.husain95@gmail.com>
2018-02-10 22:10:02 +0530

    changes made to database

--- app.js
+++ app.js
@@ -5,6 +5,13 @@ var mongoose=require("mongoose");
 app.use(bodyparser.urlencoded({extended:true}));
 app.set("view engine","ejs");
 
+var campgroundSchema= new mongoose.Schema({
+  name:String,
+  image:String
+});
+
+var Campground=mongoose.model("Campground",campgroundSchema)
+
 mongoose.connect("mongodb://localhost/yelp_camp")
 var campgrounds=[
   {name:"salmon creek",image:"https://images.unsplash.com/photo-1487730116645-74489c95b41b?auto=format&fit=crop&w=750&q=60&ixid=dW5zcGxhc2guY29tOzs7Ozs%3D"},
@@ -16,15 +23,27 @@ app.get("/",function(req,res){
   res.render("landing")
 })
 app.get("/campgrounds",function(req,res){
-  res.render("campgrounds",{campgrounds:campgrounds})
-})
+  Campground.find({},function(err,allCampgrounds){
+  if(err){
+    console.log(err);
+  }else{
+    res.render("campgrounds",{campgrounds:allCampgrounds})
+  }
+});
+});
 app.post("/campgrounds",function(req,res){
   var name=req.body.name;
   var image=req.body.image;
   var newCampground={name:name,image:image}
-  campgrounds.push(newCampground)
-  res.redirect("/campgrounds")
+  Campground.create(newCampground,function(err,newlycreated)){
+    if(err)
+    {
+      console.log(err);
+    }else{
+      res.redirect("/campgrounds")
+    }
 })
+});
 app.get("/campgrounds/new",function(req,res){
   res.render("new.ejs")
 })
